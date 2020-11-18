bda9da1256241e9b87db89eaef32615df9299f20
Jenny Zhu <jz2915@nyu.edu>
2020-11-19 02:45:20 +0800

    Nurse view appt frontend DONE; start view past mc

--- EHR/static/js/nurseViewAppt.js
+++ EHR/static/js/nurseViewAppt.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // $("select").empty();
+    $("select").empty();
     // init form
     var mcID = $("#mcID").text();
     $.ajax({
@@ -7,24 +7,53 @@ $(document).ready(function() {
       type: 'POST',
       data: {"mcID": mcID},
       success: function(res){
-        // $("#bodyTemperature").text(res.preExam.bodyTemperature);
-        // $("#pulseRate").text(res.preExam.pulseRate);
-        // $("#bloodPressure").text(res.preExam.bloodPressure);
-        // $("#diagnosis").text(res.diagnosis);
-        // for (let i=0; i < res.prescripitions.length; i++) {
-        //   $("#prescriptions").append(newPrescriptionCard(i, prescripitions[i].id + ": " + prescripitions[i].medicine, prescripitions[i].dose, prescripitions[i].comments))
-        // }
-        // for (let i=0; i < res.labReportTypes.length; i++) {
-        //   $(this).append(new Option(res.labReportTypes[i].typeName, res.labReportTypes[i].typeID))
-        // }
-        // for (let i=0; i < res.labReports.length; i++){
-        //   $("#labReports").append(newLabReportCard(i, labReports[i].id + ": " + labReports[i].lr_type, labReports[i].id, labReports[i].comments))
-        // }
+        $("#bodyTemperature").text(res.preExam.bodyTemperature);
+        $("#pulseRate").text(res.preExam.pulseRate);
+        $("#bloodPressure").text(res.preExam.bloodPressure);
+        $("#diagnosis").text(res.diagnosis);
+        for (let i=0; i < res.prescripitions.length; i++) {
+          $("#prescriptions").append(newPrescriptionCard(i+1, prescripitions[i].id + ": " + prescripitions[i].medicine, prescripitions[i].dose, prescripitions[i].comments));
+        }
+        for (let i=0; i < res.labReportTypes.length; i++) {
+          $(this).append(new Option(res.labReportTypes[i].typeName, res.labReportTypes[i].typeID));
+        }
+        for (let i=0; i < res.labReports.length; i++){
+          $("#labReports").append(newLabReportCard(i+1, labReports[i].lr_type, labReports[i].id, labReports[i].comments));
+        }
       },
       error: function(err) {
         console.log(err);
       }
     });
+
+    //--------------get the lab report-----------------
+    $(".lr-btn").on("click", function(event){
+      const targetCard = $(this).attr("data-target");
+      const button = $(targetCard+" a");
+      // if (button.attr('href') == ""){
+      const lrID = button.attr('id').slice(6);
+      $.ajax({
+        url: "http://localhost:5000/nursePreviewLR",
+        type: 'POST',
+        data: {"lrID": lrID},
+        success: function(res){
+          if (res.labReport) {
+            button.attr('href', URL.createObjectURL(res.labReport));
+          }
+        },
+        error: function(err) {
+          console.log(err);
+        }
+      });
+      // }
+    });
+
+    $(".preview-btn").on("click", function(event){
+      if ($(this).attr('href') == "") {
+        alert("Lab report empty");
+        event.preventDefault();
+      }
+    })
 
     //--------------------forms--------------------
     $("#editPreExam").on("click", function(event){
@@ -39,6 +68,12 @@ $(document).ready(function() {
         data: data,
         success: function(res){
           console.log(res);
+          if (res.ret == "0") {
+            window.location.replace("http://localhost:5000/nurseViewAppt/" + $("#appID").text());
+          }
+        },
+        error: function(err) {
+          console.log(err);
         }
       })
     });
@@ -55,6 +90,12 @@ $(document).ready(function() {
         data: data,
         success: function(res){
           console.log(res);
+          if (res.ret == "0") {
+            window.location.replace("http://localhost:5000/nurseViewAppt/" + $("#appID").text());
+          }
+        },
+        error: function(err) {
+          console.log(err);
         },
         cache: false,
         processData: false,
@@ -101,7 +142,7 @@ function newPrescriptionCard(index, idAndMedicine, dose, comments){
   return card
 }
 
-function newLabReportCard(index, idAndType, id, comments){
-  var card = "<div class='card card-body'> <h5 class='mb-0'> <button class='btn btn-link' data-toggle='collapse' data-target='#lab" + index + "' aria-expanded='true' aria-controls='lab" + index + "'>" + idAndType + "</button> </h5> <div id='lab" + index + "' class='collapse' aria-labelledby='lab" + index + "' data-parent='#labReports'> <div class='card-body'>" + id + "</div> <div class='card-body'>" + comments + "</div> </div> </div>";
+function newLabReportCard(index, type, id, comments){
+  var card = "<div class='card card-body'> <h5 class='mb-0'> <button class='lr-btn btn btn-link' data-toggle='collapse' data-target='#lab" + index + "' aria-expanded='true' aria-controls='lab" + index + "'>" + index + ". " + type + "</button> </h5> <div id='lab" + index + "' class='collapse' aria-labelledby='lab" + index + "' data-parent='#labReports'> <hr> <div class='card-body'><span>Lab report id: " + id + " </span><a id='preview" + id + "' class='preview-btn btn btn-sm btn-outline-primary' href='' target='_blank'>preview</a> </div> <div class='card-body'>" + comments + "</div> </div> </div>";
   return card
 }
