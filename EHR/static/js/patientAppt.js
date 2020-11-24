54a55c8001773f09e7aff3e422d28eb8dd5c4850
SZTankWang <2938599137@qq.com>
2020-11-24 13:38:18 +0800

    mychange

--- EHR/static/js/patientAppt.js
+++ EHR/static/js/patientAppt.js
@@ -18,6 +18,10 @@
 		// console.log($(this).text());
 		var dept = $(this).text();
 		console.log(dept);
+		var deptID = $(this).attr("id");
+		console.log(deptID);
+		loadDoctorByDept(deptID);
+		// loadDoctorByDept()
 		// var hospitalID = 
 		// ajax请求
 		// $.ajax({
@@ -41,6 +45,9 @@
 			data:{'deptID':deptID},
 			success:function(data){
 				console.log(data);
+				for(var i=0;i<data.length;i++){
+
+				}
 			}
 
 		})
@@ -48,8 +55,9 @@
 
 	}
 
-	function renderDoctor(data){
-		
+	function renderDoctor(doctor){
+		var temp = '';
+		temp += '<div class="row w-100"><div class="col"><div class="card"><div class="card-body"><div class="row w-100"><div class="doctor-avatar col-md-2"><svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/></svg></div><div class="card-text col-md-6"><p>Dr.&nbsp'+doctor['doctorName']+'</p>';
+		temp += '<p>'+doctor['hostpital']+' '+doctor['department']+'</p></div><a href="#" class="btn btn-primary col-md-2 offset-md-2 appoint-button" id='+doctor['id']+'>Appoint</a></div></div></div></div>'
 	}
 
-'<div class="row w-100"><div class="col"><div class="card"><div class="card-body"><div class="row w-100"><div class="doctor-avatar col-md-2"><svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/></svg></div><div class="card-text col-md-6"><p>Dr.John Smith</p><p>Oriental Hospital  Emergency Department</p></div><a href="#" class="btn btn-primary col-md-2 offset-md-2 appoint-button">Appoint</a></div></div></div></div>'
